42a99ef3703e5eaa172b268c603226d39ca9b809
Lukas Laudrain <lukas.ldrn@gmail.com>
2024-11-17 19:50:58 +0100

    feat: cta animation

--- src/components/features/hero/headings.tsx
+++ src/components/features/hero/headings.tsx
@@ -1,6 +1,8 @@
+'use client';
+
 import React, { Children, ReactNode, useLayoutEffect } from 'react';
 
-import { motion, stagger, useAnimate } from 'motion/react';
+import { easeInOut, motion, stagger, useAnimate } from 'motion/react';
 
 import Button from '@/element/button';
 import Heading1 from '@/element/heading1';
@@ -45,6 +47,24 @@ const AnimatedSlideHeading = ({ children }: IPropsWithChildren) => {
   );
 };
 
+const AnimatedPopButton = ({ children }: IPropsWithChildren) => {
+  return (
+    <Button
+      initial={{ opacity: 0, scale: 1.2, background: 'rgb(2 8 30 / 0)', color: 'rgb(2 8 30)' }}
+      animate={{ opacity: 1, scale: 1, background: 'rgb(2 8 30 / 1)', color: 'rgb(254 248 241)' }}
+      transition={{
+        opacity: { duration: 0.6, delay: 2.5, easings: easeInOut },
+        scale: { duration: 0.6, delay: 2.5, easings: easeInOut },
+        background: { duration: 0.5, delay: 2.7, easings: easeInOut },
+        color: { duration: 0.3, delay: 2.7, easings: easeInOut },
+      }}
+      className="mt-3"
+    >
+      {children}
+    </Button>
+  );
+};
+
 const Headings = () => {
   return (
     <div className="flex flex-col items-start gap-5 pl-16 pt-16">
@@ -54,14 +74,7 @@ const Headings = () => {
         Streamlining operations with custom web apps tailored to your business needs
       </AnimatedBlurHeading>
 
-      <Button
-        initial={{ opacity: 0, color: 'rgb(254 248 241 / 0)' }}
-        animate={{ opacity: 1, color: 'rgb(254 248 241 / 1)' }}
-        transition={{ opacity: { duration: 0.7, delay: 2.1, easings: 'easeIn' }, color: { duration: 0.7, delay: 2.3 } }}
-        className="mt-3"
-      >
-        Let&apos;s talk
-      </Button>
+      <AnimatedPopButton>Let&apos;s talk</AnimatedPopButton>
     </div>
   );
 };
