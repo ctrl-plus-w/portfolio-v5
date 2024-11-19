8e727c336a754285bb275b51ab35d6976fa3807e
Lukas Laudrain <lukas.ldrn@gmail.com>
2024-11-19 10:38:49 +0100

    feat: responsive footer

--- src/components/features/footer/footer.tsx
+++ src/components/features/footer/footer.tsx
@@ -25,7 +25,10 @@ export interface FooterProps {}
 
 const Footer = forwardRef<HTMLDivElement, FooterProps>((_, ref) => {
   return (
-    <footer ref={ref} className="bg-primary-darker fixed top-full z-20 grid w-svw grid-cols-2 gap-x-32 gap-y-8 p-24">
+    <footer
+      ref={ref}
+      className="fixed top-full z-20 flex w-svw flex-col gap-x-32 gap-y-8 bg-primary-darker p-8 py-12 md:grid md:grid-cols-2 md:p-24"
+    >
       <Heading1 className="text-secondary">
         Looking to collaborate or have questions? Reach out — I’d love to connect and create something great together
       </Heading1>
@@ -33,7 +36,7 @@ const Footer = forwardRef<HTMLDivElement, FooterProps>((_, ref) => {
       <div className="col-start-2 flex flex-col gap-5">
         <Heading2 className="uppercase text-secondary/60">Contact</Heading2>
 
-        <div className="grid grid-cols-2 grid-rows-2 gap-4">
+        <div className="grid grid-cols-1 grid-rows-4 gap-4 md:grid-cols-2 md:grid-rows-2">
           <ContactItem label="Github" href="https://github.com/ctrl-plus-w" />
           <ContactItem label="Email" href="mailto:lukas.ldrn@gmail.com" hrefLabel="lukas.ldrn@gmail.com" />
           <ContactItem label="LinkedIn" href="www.linkedin.com/in/lukaslaudrain" />
