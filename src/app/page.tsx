b542f1716b001c2664b8b80040955f07588debf3
Lukas Laudrain <lukas.ldrn@gmail.com>
2024-11-19 11:00:38 +0100

    feat: favicon & metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,4 @@
-import { Metadata } from 'next';
-
 import LandingPage from '@/feature/landing/landing-page';
-
-export const metadata: Metadata = {
-  title: 'Lukas Laudrain • Full-Stack Developer',
-  description: 'Streamlining operations with custom web apps tailored to your business needs.',
-};
 
 const Page = () => {
   return <LandingPage />;
